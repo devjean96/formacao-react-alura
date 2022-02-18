3323d18f913f7b735d6d42fba3c76065c49fe3a0
Jean Pereira Moreira - JPR <devjean96@gmail.com>
2022-02-18 14:28:36 -0300

    projetos da formação react no alura

---
+++ ceep-app/src/App.js
@@ -0,0 +1,36 @@
+import { Component } from "react";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import ListaDeNotas from "./components/ListadeNotas/ListaDeNotas";
+import "./assets/App.css";
+import "./assets/index.css";
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
+      </section>
+    );
+  }
+
+}
+
+export default App;
