ffa07da4050f286c155607f857a583ec7c368da9
Jean Pereira Moreira - JPR <devjean96@gmail.com>
2022-02-18 20:18:12 -0300

    criacao lista de categorias

--- ceep-app/src/App.js
+++ ceep-app/src/App.js
@@ -3,30 +3,31 @@ import FormularioCadastro from "./components/FormularioCadastro/FormularioCadast
 import ListaDeNotas from "./components/ListadeNotas/ListaDeNotas";
 import "./assets/App.css";
 import "./assets/index.css";
+import ListaDeCategorias from "./components/ListaDeCategorias/ListaDeCategorias";
 
 class App extends Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      notas:[]
+      notas: []
     }
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas,novaNota]
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota]
     const novoEstado = {
-      notas:novoArrayNotas
+      notas: novoArrayNotas
     }
     this.setState(novoEstado)
   }
 
-  deletarNota(index){
+  deletarNota(index) {
     let arrayNotas = this.state.notas;
-    arrayNotas.splice(index,1);
+    arrayNotas.splice(index, 1);
     this.setState({
-      notas:arrayNotas
+      notas: arrayNotas
     })
     console.log("deletar")
   }
@@ -34,10 +35,13 @@ class App extends Component {
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas 
-        apagarNota={this.deletarNota.bind(this)}
-        notas={this.state.notas}/>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <main className="conteudo-principal">
+          <ListaDeCategorias/>
+          <ListaDeNotas
+            apagarNota={this.deletarNota.bind(this)}
+            notas={this.state.notas} />
+        </main>
       </section>
     );
   }
